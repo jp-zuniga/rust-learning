fae30d89d3067e360580679fc0e9dcd2cbb1251d
jp-zuniga <jperez070705@gmail.com>
2025-05-28 22:19:51 -0600

    more ownership, holy shit

--- ch-4/ownership/src/main.rs
+++ ch-4/ownership/src/main.rs
@@ -20,21 +20,21 @@ fn main() {
         s.push_str(", world!");
         println!("{s}");
 
-        // the value of 5 is copied and both variables are pushed to the stack
-        // ! ---------------------------------------------------------------------
+        // * the value of 5 is copied and both variables are pushed to the stack
+        // * ---------------------------------------------------------------------
         // ? let x = 5;
         // ? let y = x;
 
-        // s2 receives a copy of s1's 'metadata' (pointer, length, capacity),
-        // but it still points to the same address on the heap that holds s1's value
-        // ! ---------------------------------------------------------------------
+        // * s2 receives a copy of s1's 'metadata' (pointer, length, capacity),
+        // * but it still points to the same address on the heap that holds s1's value
+        // * ---------------------------------------------------------------------
         // ? let s1 = String::from("hello");
         // ? let s2 = s1;
-        // ! ---------------------------------------------------------------------
+        // * ---------------------------------------------------------------------
 
-        // however, in this case, s1 is invalidated when s2 is created
-        // if it wasn't, s1 and s2 would both try to free the same memory
-        // on the heap when they go out of scope at the same time (double free error)
+        // however, in this case, s1 is invalidated when s2 is created.
+        // if it wasn't, s1 and s2 would both go out of scope at the same time,
+        // and try to free the same memory at the same time (double free error).
 
         // the invalidation of s1 means s2 isn't a copy at all:
         // the data bound to s1 is *moved* to s2, who becomes its owner
@@ -49,23 +49,26 @@ fn main() {
     // ? s = String::from("ahoy");
 
     // here, s is immediately assigned a new value,
-    // which means no one owns the original "hello" value,
-    // so rust considers it to have gone out of scope, and frees its memory
+    // which means no one owns the original "hello" value anymore,
+    // so rust considers it to have gone out of scope and frees its memory
 
     // ? println!("{s}, world!");
     // * ahoy, world!
 
     // ! ---------------------------------------------------------------------
 
-    // since an int's size is known at compile time, it's pushed to the stack
-    // that means it's copying a value and pushing it again is cheap, so
-    // rust doesn't bother invalidating x after it gets copied into y
+    // since an int's size is known at compile time, it's pushed to the stack.
+    // copying its value and pushing it again is cheap, so rust doesn't bother
+    // invalidating x after it gets copied into y; both own separate items
+    // on the stack, that happen to have the same value copied across.
     let x = 5;
     let y = x;
 
-    // under the hood, simple types that don't require allocations implement Copy
-    // anything that can change at runtime and is allocated on the heap
-    // has to implement Drop and is banned from implemented Copy
+    // under the hood, simple types that don't require allocations implement Copy.
+
+    // anything that can change at runtime and is allocated on the heap *can't*
+    // implement Copy, and must instead implement Drop, to handle memory
+    // collection when the value goes out of scope.
 
     // Copy and Drop are 'traits', which sound like dunder methods but who knows
 
@@ -93,6 +96,28 @@ fn main() {
 
     // this would cause a compile error:
     // println!("{s}");
+
+    // ! ---------------------------------------------------------------------
+
+    {
+        // the value returned by gives_ownership() is moved into
+        // the current scope, and the new variable takes ownership of it
+        let visitor = gives_ownership();
+
+        // a new string comes into scope
+        let new_string = String::from("i'm new!");
+
+        // and is then moved to the scope of the called function,
+        // who then returns it back here so it may live on
+        let still_kicking  = takes_and_gives_back(new_string);
+
+        // new_string no longer owns a value, so accessing it would cause an error
+        println!("{visitor} + {still_kicking}!");
+    }
+
+    // after the scope ends, visitor and still_kicking are dropped,
+    // but since new_string was *moved* after being given to the function,
+    // still_kicking took on ownership, so nothing happens to new_string
 }
 
 
@@ -107,4 +132,23 @@ fn makes_copy(some_int: i32) {
     // some_int comes into scope
     println!("{some_int}");
     // then leaves and is dropped
+}
+
+
+fn gives_ownership() -> String {
+    // some_string is created
+    let some_string = String::from("yours!");
+
+    // and is sent off back to whoever called the function,
+    // out of this scope so it may live on in another
+    return some_string;
+}
+
+
+fn takes_and_gives_back(some_string: String) -> String {
+    // some_string comes into scope
+    // stuff is done with it
+
+    // it's sent back to whence it came
+    return some_string;
 }
